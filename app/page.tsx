133bdd69310e231a9f4e9e233c172a986b5f4c18
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-04-23 15:26:31 -0500

    Update homepage

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+export const metadata = {
+  title: "Home - Open PRO",
+  description: "Page description",
+};
+
+import Hero from "@/components/hero-home";
+import Process from "@/components/process";
+import FeaturesHome from "@/components/features";
+import Tabs from "@/components/tabs";
+import Target from "@/components/target";
+import Newsletter from "@/components/newsletter";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Process />
+      <FeaturesHome />
+      <Tabs />
+      <Target />
+      <Newsletter />
+    </>
+  );
+}
