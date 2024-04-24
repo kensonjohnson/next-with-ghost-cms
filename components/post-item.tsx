10982b65d291ef0d442428e987a4c829980e658e
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-04-23 20:32:03 -0500

    Refactor to use ghost posts

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -2,47 +2,52 @@ import Link from "next/link";
 import Image from "next/image";
 import PostDate from "@/components/post-date";
 import PostTags from "./post-tags";
+import { PostOrPage } from "@tryghost/content-api";
 
-export default function PostItem({ ...props }) {
+type PostItemProps = {
+  post: PostOrPage;
+};
+
+export default function PostItem({ post }: PostItemProps) {
   return (
     <article className="flex flex-col h-full" data-aos="fade-up">
       <header>
-        {props.image && (
-          <Link href={`/blog/${props.slug}`} className="block mb-6">
+        {post.feature_image && (
+          <Link href={`/blog/${post.slug}`} className="block mb-6">
             <figure className="relative h-0 pb-9/16 overflow-hidden rounded-sm">
               <Image
                 className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition duration-700 ease-out"
-                src={props.image}
+                src={post.feature_image}
                 width={352}
                 height={198}
-                alt={props.title}
+                alt={post.feature_image_alt ?? ""}
               />
             </figure>
           </Link>
         )}
-        {props.tags && (
+        {post.tags && (
           <div className="mb-3">
-            <PostTags tags={props.tags} />
+            <PostTags tags={post.tags} />
           </div>
         )}
         <h3 className="h4 mb-2">
           <Link
-            href={`/blog/${props.slug}`}
+            href={`/blog/${post.slug}`}
             className="hover:text-gray-100 transition duration-150 ease-in-out"
           >
-            {props.title}
+            {post.title}
           </Link>
         </h3>
       </header>
-      <p className="text-lg text-gray-400 grow">{props.summary}</p>
+      <p className="text-lg text-gray-400 grow">{post.excerpt}</p>
       <footer className="flex items-center mt-4">
         <Link href="#">
-          <img
+          <Image
             className="rounded-full shrink-0 mr-4"
-            src={props.authorImg}
+            src={post.authors?.at(0)?.profile_image ?? ""}
             width={40}
             height={40}
-            alt={props.author}
+            alt={post.authors?.at(0)?.name ?? ""}
           />
         </Link>
         <div className="font-medium">
@@ -50,11 +55,11 @@ export default function PostItem({ ...props }) {
             href="#"
             className="text-gray-200 hover:text-gray-100 transition duration-150 ease-in-out"
           >
-            {props.author}
+            {post.authors?.at(0)?.name ?? ""}
           </Link>
           <span className="text-gray-700"> - </span>
           <span className="text-gray-500">
-            <PostDate dateString={props.publishedAt} />
+            <PostDate dateString={post.published_at ?? ""} />
           </span>
         </div>
       </footer>
--- components/post-tags.tsx
+++ components/post-tags.tsx
@@ -1,15 +1,16 @@
+import { Tag } from "@tryghost/content-api";
 import Link from "next/link";
 
-export default function PostTags({ tags }: { tags: string[] }) {
+export default function PostTags({ tags }: { tags: Tag[] }) {
   const tagColor = (tag: string) => {
     switch (tag) {
-      case "Engineering":
+      case "News":
         return "text-gray-100 bg-blue-500 hover:bg-blue-600";
-      case "Design":
+      case "Test":
         return "text-gray-100 bg-pink-500 hover:bg-pink-600";
       case "Tutorials and articles":
         return "text-gray-100 bg-teal-500 hover:bg-teal-600";
-      case "Culture":
+      case "Health":
         return "text-gray-100 bg-green-500 hover:bg-green-600";
       default:
         return "text-gray-100 bg-purple-600 hover:bg-purple-700";
@@ -22,9 +23,11 @@ export default function PostTags({ tags }: { tags: string[] }) {
         <li key={tagIndex} className="m-1">
           <Link
             href="#"
-            className={`inline-flex text-center py-1 px-3 rounded-full transition duration-150 ease-in-out ${tagColor(tag)}`}
+            className={`inline-flex text-center py-1 px-3 rounded-full transition duration-150 ease-in-out ${tagColor(
+              tag.name!
+            )}`}
           >
-            {tag}
+            {tag.name}
           </Link>
         </li>
       ))}
